0cc93f3ac67b671fa780cd098a2d2fd52012498c
amyjording <zanizm@gmail.com>
2019-02-04 11:31:38 -0800

    About Me

--- view/static.py
+++ view/static.py
@@ -18,7 +18,7 @@ def index_template():
 				{0}
 
 		<!-- Dynamic svg text content here -->
-		{1} <a id="shutdown"; href="./shutdown">Shutdown Server</a>
+		{1} <!-- <a id="shutdown"; href="./shutdown">Shutdown Server</a> -->
 
 		<div class="container-fluid">
 		  <div class="wrapper">
@@ -57,39 +57,65 @@ def aboutme_template():
 					<div class="col"><h1>About Me</h1></div>
 				</div>
 				<div class="row">
-					<div class="col"><p>Back in 1997, I took my first step with development. Thanks to Angelfire and Geocities, 
+					<div class="col about"><h4>In 1996, I took my first step with development. Thanks to Angelfire and Geocities, 
 					I learned the magic of HTML, and can't begin to tell you how utterly baffled I was at the concept of 
-					magically getting my picture from my computer out onto this page that wasn't living on my computer. "Upload",
-					I would later understand it to be.</p></div>
-					<div class="col"><p>In 2004, I got acquainted with Wordpress. It was a blogging platform, but it didn't
-					take long for me to see that a 'blog' and a 'page', were more or less, just labels and that one was a
-					static behavior, while the other was dynamic. I didn't know those terms yet, but I had a feeling the sheer
-					simplicity of how it was built, was a handy tool.</p></div>
+					magically getting my picture from my computer out onto this page that was 'somewhere out there'. When I finally
+					grasped the concept of uploading, it opened up a world of possibilities in my mind.</h4><h4>In 2001, I took an internship with a local media company. There,
+					I applied my HTML and CSS knowledge on a few projects, and even became familiar with Adobe Premiere as well as
+					Macromedia Flash Player. I got a taste of understanding layers and got my feet wet with "keys". It was a great
+					learning experience over the course of 6 months.</h4>
+					<h4>In 2004, I discovered Wordpress. It opened my eyes to 
+					the concept of a template, and helped me to comprehend the idea of keeping data stored separately from
+					the visual interface. While at the time, I still mostly used it for blogging, I enjoyed exploring the 
+					nuts and bolts of how it worked. Eventually, I wound up buying my own hosting account in 2007, and consequently
+					explored the nooks and crannies such as CPanel, phpMyAdmin, ftp, filemanager, etc.</h4></div>
 				</div>
 				<div class="row">
-					<div class="col"><p>Throughout the years, on a professional scale, I mainly stuck to office support jobs. 
-					I wanted to take my web knowledge further, but I really hadn't heard of many of the terms other than php,
-					and to be honest, it was a nightmare code to try to dissassemble.</p><p> I did, however, get better
-					acquainted with Adobe Create Suite, and got up to speed on the new concept of Responsive Design.</p> 
-					<p>Life went on for a while, and I decided
-					to volunteer on a farm for a Non-Profit in the Columbia Gorge area. I discovered a deep love for going 
-					beyond the label of "sustainable", but pushed myself further into bountifully abundant due to a concept
-					called permaculture.</p></div>				
+					<div class="col about"><h4>Throughout the span of the 2000s, I mainly worked in some form of supportive position. 
+					From mastering Espresso (came in 5th at a Barista competition in Seattle back in 2000), to a temporary job at a banking 
+					call center, from an assistant at a Diode Laser manufacturer, to another at an Aerospace parts distributer; from working on storage facility security 
+					software maps with PaintShop Pro (2007), to Assistant Support Person for Kroger HQ (2008-2010), I've encompassed 
+					quite a variety of "on the job" skills.</h4>
+				</div>				
 				</div>
 				<div class="row">
-					<div class="col"><p>It's a passion that I plan on delving into for many years going forward. But I knew
-					that the first step toward this bountiful sustaining lifestyle started with me. So I rolled up my sleeves 
-					and committed to learning how to really develop. I got started with The Odin Project in 2016, and didn't 
-					speed ahead to the Rails chapter. I made myself push through the hardest parts of Ruby (like building a chess game).</p></div>
-					<div class="col"><p>In 2017, I caught a break with a great Non-profit company called Keystone Accountability. 
-					I started as a "Tech Fellow" (what I lovely dub, a "Pre-Junior Developer"), and felt the sting of hot coals 
-					on my digital feet as I dove head into Python, Cherrypy and Drupal. I even wound up developing their user 
-					login system so that we could detach Drupal, which was the user management portion of the site.</p></div>
+					<div class="col about"><h4>In 2010, I started a blog called Arabian Glimmer. I have always been a big fan of
+					the Arabian horse breed, and had a knack for remembering hundreds of horses and even memorizing pedigrees. So
+					I decided to take this to a more public forum and follow my heart.</h4>
+					<h4> By 2011, I wanted to gain a better understanding
+					of website/user interface styling, and so I decided to basically reverse engineer an existing Wordpress template
+					and make it my own, while learning different styling techniques.</h4>
+					</div>
 				</div>
 				<div class="row">
-					<div class="col"><p>I've had a unique experience in that I've learned the convention and convenience of Rails,
-					but have come to really find more 'fun' from developing in pure Python using a minimalist framework like Cherrypy.
-					</p></div>				
+					<div class="col about"><h4>From 2012 through 2017, I was essentially on a bit of a pre mid-life sabbatical. I 
+					immersed myself in sustainable farm-life and the concept of Permaculture while doing an internship with a
+					non-profit called World Steward, in the Columbia Gorge. I hauled hay, learned how to be a steward of the forest (Scotch broom is 
+		            awful!), and grew a wide variety of crops. <br /><br />
+		            Eventually, I came back to the regular working world for a short time and worked as seasonal Quality Assurance inspector
+		            for a print production company, and not long after that, enjoyed three years in Ensenada, BC, Mexico.</h4>
+		            </div>
+		        </div>
+				<div class="row">
+					<div class="col about">        
+					<h4>In 2016, I finally embraced the path of a Full Stack Web Developer. I rolled up my sleeves and began my self-taught
+					 journey with <a href="https://www.theodinproject.com/">The Odin Project</a>. I took the long road and committed to understanding how to program with Ruby. What I was
+					really doing was setting the foundation for what it takes to develop. Try, Fail, Research, Try again, Fail again
+					(with great enthusiasm because the failure is new!), rinse and repeat until I would reach the finish line.</h4>
+					<h4>Toward the end of 2016, I still wasn't completely finished with the Rails portion of
+					The Odin Project, but I was actively looking for some sort of stepping stone toward being able to bring an income
+					in and possibly keep learning. I found an opportunity and applied.</h4>
+					<h4> After about three months of dead silence, I 
+					was completely surprised with a response from a great Non-profit company, Keystone Accountability. 
+					I started as a "Tech Fellow" (what I affectionately dub, a "Pre-Junior Developer"), and felt the sting of hot coals 
+					on my digital feet as I faced my trial by fire, first into Python, Cherrypy, Drupal, Bootstraps, Ajax, and JavaScript. </h4>
+					<h4>
+					I learned a great deal about the Linux system through SSH (though I began most of my learning with Windows, so I had extra 
+					hills to climb), learned the importance of staging and development, wrestled with version control with Github, and grew to
+					have my own database preference (I adore MongoDB). I even wound up developing their user login system so that we could detach from Drupal, which was the user management portion of the site.</h4>
+					<h4>After working for them for a year, they promoted me to Web Developer. All the while, I've been committed to continued learning
+					and absolutely had to build this portfolio from scratch before I felt ready to reach out and find my (hopefully) permanent
+					home for the rest of my career.<br /><br /></div>
 				</div>
 		  	</div>
 		</div>
@@ -116,18 +142,18 @@ def contactme_template():
 					<div class="col"><h1>Contact Me</h1></div>
 				</div>
 				<div class="row">
-					<div class="col"><p>Linked In</p><p>Twitter</p><p>Github</p></div>
+					<div class="col"><h4>Linked In</h4><h4>Twitter</h4><h4>Github</h4></div>
 					<div class="col"></div>
 				</div>
 				<div class="row">
-					<div class="col"><p>Email Contact Form</p></div>				
+					<div class="col"><h4>Email Contact Form</h4></div>				
 				</div>
 				<div class="row">
-					<div class="col"><p></p></div>
-					<div class="col"><p></p></div>
+					<div class="col"><h4></h4></div>
+					<div class="col"><h4></h4></div>
 				</div>
 				<div class="row">
-					<div class="col"><p></p></div>				
+					<div class="col"><h4></h4></div>				
 				</div>
 		  	</div>
 		</div>
